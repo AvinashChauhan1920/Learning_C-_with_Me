f5c74bf50af314cf45548685a72a26271c81c60a
Avinash Chauhan <121370151+AvinashChauhan1920@users.noreply.github.com>
2024-11-05 21:55:52 +0530

    Update L001_Basic_Syntax.cpp

--- L001_Basic_Syntax.cpp
+++ L001_Basic_Syntax.cpp
@@ -1,7 +1,8 @@
-#include<iostream>
+#include <iostream> //Header File
 using namespace std;
 
-int main() {
-    cout<<"Hello World";
+int main() // Int for Integer and Main is function and It is point where program will started
+{
+    cout << "Hello World"; // For printing
     return 0;
 }
